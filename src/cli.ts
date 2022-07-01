6b44a226ebf21940824084478a62051590fd125a
Pierrick Turelier <pierrick@turelier.com>
2022-07-01 12:01:59 -0500

    fix(cli): destructure env object

--- src/cli.ts
+++ src/cli.ts
@@ -2,13 +2,14 @@ import { handler } from './handler'
 
 if (require.main === module) {
   const {
-    RELAYER_API_KEY,
-    RELAYER_API_SECRET,
+    RELAYER_API_KEY: apiKey,
+    RELAYER_API_SECRET: apiSecret,
     ETHEREUM_RINKEBY_PROVIDER_URL: ethereumRinkebyProviderURL,
   } = process.env;
+
   handler({
-    apiKey: RELAYER_API_KEY,
-    apiSecret: RELAYER_API_SECRET,
+    apiKey,
+    apiSecret,
     secrets: {
       ethereumRinkebyProviderURL,
     }
